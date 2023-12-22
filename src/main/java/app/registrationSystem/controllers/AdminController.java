e0f785e1570d24afb5eeffa8b235850abd1397ff
Patryk Podgórski <podgorski.p@interia.eu>
2023-12-22 18:39:41 +0100

    Removes @Transactional from controller level

--- src/main/java/app/registrationSystem/controllers/AdminController.java
+++ src/main/java/app/registrationSystem/controllers/AdminController.java
@@ -1,13 +1,14 @@
 package app.registrationSystem.controllers;
 
 import app.registrationSystem.dto.DoctorDTO;
+import app.registrationSystem.dto.Response;
 import app.registrationSystem.security.CustomUserDetails;
 import app.registrationSystem.security.Privilege;
 import app.registrationSystem.security.RequiredPrivilege;
 import app.registrationSystem.services.DoctorService;
 import app.registrationSystem.services.PatientService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,45 +22,24 @@ public class AdminController {
     private final DoctorService doctorService;
     private final PatientService patientService;
 
-    @Transactional
     @RequiredPrivilege(value = Privilege.REMOVE_DOCTOR)
     @DeleteMapping("/remove-doctor/{id}")
-    public ResponseEntity<String> removeDoctor(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long id) {
-
-        Optional<Long> doctorID = doctorService.removeDoctor(id);
-
-        if (doctorID.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user of the id " + id + " found");
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body("Correctly removed the user of the id " + id);
+    public ResponseEntity<Response> removeDoctor(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long id) {
+        Response response = doctorService.removeDoctor(id);
+        return ResponseEntity.status(response.httpStatus()).body(response);
     }
 
-    @Transactional
     @RequiredPrivilege(value = Privilege.REMOVE_PATIENT)
     @DeleteMapping("/remove-patient/{id}")
-    public ResponseEntity<String> removePatient(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long id) {
-
-        Optional<Long> patientID = patientService.removePatient(id);
-
-        if (patientID.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user of the id " + id + " found");
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body("Correctly removed the user of the id " + id);
+    public ResponseEntity<Response> removePatient(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("id") Long id) {
+        Response response = patientService.removePatient(id);
+        return ResponseEntity.status(response.httpStatus()).body(response);
     }
 
-    @Transactional
     @RequiredPrivilege(value = Privilege.ADD_DOCTOR)
     @PostMapping("/add-doctor")
-    public ResponseEntity<String> addDoctor(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody DoctorDTO doctorDTO){
-
-        Optional<Long> doctorID = doctorService.addDoctor(doctorDTO);
-
-        if (doctorID.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username: " + doctorDTO.getUsername() + " is already taken");
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body("Correctly created a user account");
+    public ResponseEntity<Response> addDoctor(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody DoctorDTO doctorDTO){
+        Response response = doctorService.addDoctor(doctorDTO);
+        return ResponseEntity.status(response.httpStatus()).body(response);
     }
 }
