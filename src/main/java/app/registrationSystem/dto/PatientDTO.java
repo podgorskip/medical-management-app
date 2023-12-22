2c75082c0cce3eb76a66b97625c7d29d1725b05b
Patryk Podgórski <podgorski.p@interia.eu>
2023-12-22 18:23:30 +0100

    Adds additional field to dto

--- src/main/java/app/registrationSystem/dto/PatientDTO.java
+++ src/main/java/app/registrationSystem/dto/PatientDTO.java
@@ -1,13 +1,15 @@
 package app.registrationSystem.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import java.util.Set;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class PatientDTO extends UserDTO {
-
+    private Set<Long> illnesses;
 }
