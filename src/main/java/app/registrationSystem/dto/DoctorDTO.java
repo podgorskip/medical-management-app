9280e8146147f381fe5c92912f9ac9166ea7882b
Patryk Podgórski <podgorski.p@interia.eu>
2023-12-22 17:46:05 +0100

    Adds not arg constructor

--- src/main/java/app/registrationSystem/dto/DoctorDTO.java
+++ src/main/java/app/registrationSystem/dto/DoctorDTO.java
@@ -3,10 +3,12 @@ package app.registrationSystem.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class DoctorDTO extends UserDTO {
     private Long specialization;
