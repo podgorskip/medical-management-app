0a32e9225dce487181644bc1c527109b5d4393ce
Patryk Podgórski <podgorski.p@interia.eu>
2023-12-23 15:38:15 +0100

    Adds DTO to represent visits

--- src/main/java/app/registrationSystem/dto/VisitsDTO.java
+++ src/main/java/app/registrationSystem/dto/VisitsDTO.java
@@ -1,2 +1,11 @@
-package app.registrationSystem.dto;public class VisitsDTO {
+package app.registrationSystem.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class VisitsDTO {
+    private List<VisitDTO> visits;
 }
