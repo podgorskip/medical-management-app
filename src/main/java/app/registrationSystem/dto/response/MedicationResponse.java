8da3607962bbed00751f0faf11de5de77618dc8f
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:29:50 +0100

    Adds medication dto

--- src/main/java/app/registrationSystem/dto/response/MedicationResponse.java
+++ src/main/java/app/registrationSystem/dto/response/MedicationResponse.java
@@ -1,2 +1,13 @@
-package app.registrationSystem.dto.response;public class MedicationResponse {
+package app.registrationSystem.dto.response;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class MedicationResponse {
+    private Long id;
+    private String name;
+    private int dose;
+    private String description;
+    private List<String> specializations;
 }
