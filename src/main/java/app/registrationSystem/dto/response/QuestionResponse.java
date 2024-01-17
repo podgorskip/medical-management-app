8539d8bbcb7973f7b75c266b6e1d8eae52c824d8
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:30:47 +0100

    Adds question response dto

--- src/main/java/app/registrationSystem/dto/response/QuestionResponse.java
+++ src/main/java/app/registrationSystem/dto/response/QuestionResponse.java
@@ -1,2 +1,12 @@
-package app.registrationSystem.dto.response;public class QuestionResponse {
+package app.registrationSystem.dto.response;
+
+import lombok.Data;
+
+@Data
+public class QuestionResponse {
+    private Long id;
+    private String patient;
+    private String specialization;
+    private String question;
+    private String date;
 }
