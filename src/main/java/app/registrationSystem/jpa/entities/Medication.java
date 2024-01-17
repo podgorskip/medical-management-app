d0c6bdf275eb0bc3a30f9760e2af325816082b50
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 23:32:43 +0100

    Adds new entity representing medication

--- src/main/java/app/registrationSystem/jpa/entities/Medication.java
+++ src/main/java/app/registrationSystem/jpa/entities/Medication.java
@@ -1,2 +1,39 @@
-package app.registrationSystem.jpa.entities;public class Medication {
+package app.registrationSystem.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "medication", schema = "registration_system")
+@Data
+public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double dose;
+    private String description;
+
+    @ManyToMany(mappedBy = "medications")
+    private Set<Prescription> prescriptions;
+
+    @ManyToMany(mappedBy = "medications")
+    private Set<Specialization> specializations;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Medication that = (Medication) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
