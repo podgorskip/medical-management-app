ff2af1f63d6ad2c2724e668382bd48cc5b5d0113
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:34:22 +0100

    Adds question request dto

--- src/main/java/app/registrationSystem/dto/request/QuestionRequest.java
+++ src/main/java/app/registrationSystem/dto/request/QuestionRequest.java
@@ -1,2 +1,13 @@
-package app.registrationSystem.dto.request;public class QuestionRequest {
+package app.registrationSystem.dto.request;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class QuestionRequest {
+    @NonNull
+    private Long specializationID;
+
+    @NonNull
+    private String question;
 }
