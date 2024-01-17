940e5918c6eeb1710a738d6b9d0d542d6b2e7022
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:30:14 +0100

    Adds prescription dto

--- src/main/java/app/registrationSystem/dto/response/PrescriptionResponse.java
+++ src/main/java/app/registrationSystem/dto/response/PrescriptionResponse.java
@@ -1,2 +1,16 @@
-package app.registrationSystem.dto.response;public class PrescriptionResponse {
+package app.registrationSystem.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class PrescriptionResponse {
+    private String patient;
+    private List<String> medications;
+    private String code;
+    private String description;
+    private LocalDateTime issueDate;
+    private LocalDateTime expirationDate;
+    private String doctor;
 }
