0cfc47c004d19199ae7f9022217fea06c566c724
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 23:34:55 +0100

    Adds equals and hashcode methods

--- src/main/java/app/registrationSystem/jpa/entities/AvailableVisit.java
+++ src/main/java/app/registrationSystem/jpa/entities/AvailableVisit.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "available_visit", schema = "registration_system")
@@ -19,7 +20,20 @@ public class AvailableVisit {
 
     private int duration;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "doctor_id")
     private Doctor doctor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvailableVisit that = (AvailableVisit) o;
+        return duration == that.duration && Objects.equals(id, that.id) && Objects.equals(date, that.date) && Objects.equals(doctor, that.doctor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, duration, doctor);
+    }
 }
