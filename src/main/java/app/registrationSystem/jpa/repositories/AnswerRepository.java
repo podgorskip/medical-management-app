4de29527d044bccf68d1be9e1c17c5178ae52407
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:35:30 +0100

    Adds repository to handle Answer entities

--- src/main/java/app/registrationSystem/jpa/repositories/AnswerRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/AnswerRepository.java
@@ -1,2 +1,19 @@
-package app.registrationSystem.jpa.repositories;public class AnswerRepository {
+package app.registrationSystem.jpa.repositories;
+
+import app.registrationSystem.jpa.entities.Answer;
+import app.registrationSystem.jpa.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    @Query("SELECT a FROM Answer a " +
+            "LEFT JOIN a.question q " +
+            "WHERE q.patient = :patient")
+    Optional<List<Answer>> findByPatient(@Param("patient") Patient patient);
 }
