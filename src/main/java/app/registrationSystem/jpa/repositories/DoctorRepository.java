d6badad53e6e34bc13320f874b0f001c934b69e9
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:36:01 +0100

    Adds repository to handle Doctor repository

--- src/main/java/app/registrationSystem/jpa/repositories/DoctorRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/DoctorRepository.java
@@ -4,8 +4,9 @@ import app.registrationSystem.jpa.entities.Doctor;
 import app.registrationSystem.jpa.entities.Specialization;
 import app.registrationSystem.jpa.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +14,11 @@ import java.util.Optional;
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     Optional<Doctor> findByUser(User user);
     Optional<List<Doctor>> findBySpecialization(Specialization specialization);
+
+    @Query("SELECT d FROM Doctor d " +
+            "LEFT JOIN d.answers a " +
+            "WHERE d.specialization = :specialization " +
+            "GROUP BY d.id " +
+            "ORDER BY COUNT(a) ASC")
+    Optional<Doctor> findDoctorWithLeastQuestionsInSpecialization(@Param("specialization") Specialization specialization);
 }
