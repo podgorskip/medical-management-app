76a4cf4b84afb862320c2b4be34052ebc98dbd58
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 23:33:16 +0100

    Adds new entity representing prescription

--- src/main/java/app/registrationSystem/jpa/entities/Prescription.java
+++ src/main/java/app/registrationSystem/jpa/entities/Prescription.java
@@ -1,2 +1,43 @@
-package app.registrationSystem.jpa.entities;public class Prescription {
+package app.registrationSystem.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "prescription", schema = "registration_system")
+@Data
+public class Prescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    private String description;
+
+    @Column(name = "code", unique = true)
+    private String code;
+
+    @Column(name = "issue_date")
+    private LocalDateTime issueDate;
+
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "prescription_medication",
+            joinColumns = @JoinColumn(name = "prescription_id"),
+            inverseJoinColumns = @JoinColumn(name = "medication_id")
+    )
+    private Set<Medication> medications;
 }
