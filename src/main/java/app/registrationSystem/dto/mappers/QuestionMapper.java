1183496169e9cb4601589c21a2a24ed3df60933e
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:32:43 +0100

    Adds Question mapper

--- src/main/java/app/registrationSystem/dto/mappers/QuestionMapper.java
+++ src/main/java/app/registrationSystem/dto/mappers/QuestionMapper.java
@@ -1,2 +1,23 @@
-package app.registrationSystem.dto.mappers;public class QuestionMapper {
+package app.registrationSystem.dto.mappers;
+
+import app.registrationSystem.dto.response.QuestionResponse;
+import app.registrationSystem.jpa.entities.Question;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import java.time.format.DateTimeFormatter;
+
+@Mapper
+public interface QuestionMapper {
+    QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
+
+    default QuestionResponse convert(Question question) {
+        QuestionResponse questionResponse = new QuestionResponse();
+
+        questionResponse.setId(question.getId());
+        questionResponse.setPatient(question.getPatient().getUser().fullName());
+        questionResponse.setQuestion(question.getQuestion());
+        questionResponse.setSpecialization(question.getSpecialization().getName());
+        questionResponse.setDate(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm").format(question.getQuestionDate()));
+        return questionResponse;
+    }
 }
