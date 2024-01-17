6b8deff544b392c08052fbd00d7d6c0d686e3ab4
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:36:26 +0100

    Adds repository to handle Medication enitites

--- src/main/java/app/registrationSystem/jpa/repositories/MedicationRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/MedicationRepository.java
@@ -1,2 +1,13 @@
-package app.registrationSystem.jpa.repositories;public class MedicationRepository {
+package app.registrationSystem.jpa.repositories;
+
+import app.registrationSystem.jpa.entities.Medication;
+import app.registrationSystem.jpa.entities.Specialization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+    Optional<List<Medication>> findBySpecializations(Specialization specialization);
 }
