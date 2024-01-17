57bf30095a1b623d64166df4beb4b71d602eea7a
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 16:11:37 +0100

    Restructures class

--- src/main/java/app/registrationSystem/services/VisitService.java
+++ src/main/java/app/registrationSystem/services/VisitService.java
@@ -1,24 +1,31 @@
 package app.registrationSystem.services;
 
-import app.registrationSystem.jpa.entities.AvailableVisit;
-import app.registrationSystem.jpa.entities.Illness;
-import app.registrationSystem.jpa.entities.ScheduledVisit;
+import app.registrationSystem.dto.request.VisitRequest;
+import app.registrationSystem.dto.response.Response;
+import app.registrationSystem.jpa.entities.*;
 import app.registrationSystem.jpa.repositories.AvailableVisitRepository;
 import app.registrationSystem.jpa.repositories.ScheduledVisitRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * A class that handles operations related to AvailableVisit and ScheduledVisit entities
+ */
 @Service
 @RequiredArgsConstructor
 public class VisitService {
     private final DoctorService doctorService;
+    private final PatientService patientService;
     private final IllnessService illnessService;
     private final AvailableVisitRepository availableVisitRepository;
     private final ScheduledVisitRepository scheduledVisitRepository;
+    private final MailSenderService mailSenderService;
 
     /**
      * Returns available visits based on the illness name and the visit duration needed
@@ -28,7 +35,8 @@ public class VisitService {
     public Optional<List<AvailableVisit>> getAvailableVisits(String illnessName) {
         Optional<Illness> illness = illnessService.getByName(illnessName);
 
-        if (illness.isEmpty()) return Optional.empty();
+        if (illness.isEmpty())
+            return Optional.empty();
 
         return Optional.of(doctorService.getBySpecialization(illness.get().getSpecialization())
                 .stream()
@@ -47,22 +55,6 @@ public class VisitService {
     }
 
     /**
-     * Deletes the available visit
-     * @param availableVisit available visit to be removed
-     */
-    public void deleteAvailableVisit(AvailableVisit availableVisit) {
-        availableVisitRepository.delete(availableVisit);
-    }
-
-    /**
-     * Saves scheduled visit
-     * @param scheduledVisit scheduled visit to be saved
-     */
-    public void scheduleVisit(ScheduledVisit scheduledVisit) {
-        scheduledVisitRepository.save(scheduledVisit);
-    }
-
-    /**
      * Return ScheduledVisit instance of the provided ID
      * @param id id of the scheduled visit
      * @return scheduled visit
@@ -71,4 +63,126 @@ public class VisitService {
         return scheduledVisitRepository.findById(id);
     }
 
+    /**
+     * Returns list of scheduled visits for the user of the provided username
+     * @param username username of the user to have scheduled visits returned
+     * @return list of scheduled visits
+     */
+    public List<ScheduledVisit> checkPatientScheduledVisits(String username) {
+        Optional<Patient> patient = patientService.getByUsername(username);
+
+        if (patient.isEmpty())
+            return Collections.emptyList();
+
+        return patient.get().getScheduledVisits();
+    }
+
+    /**
+     * Allows to schedule a visit
+     * @param visitID ID of the available to be set as scheduled
+     * @param username username of the user to have the visit scheduled
+     * @return response with status of the performed action
+     */
+    @Transactional
+    public Response scheduleVisit(Long visitID, String username) {
+        Optional<AvailableVisit> optionalAvailableVisit = getAvailableVisit(visitID);
+
+        if (optionalAvailableVisit.isEmpty()) {
+            return new Response(false, HttpStatus.NOT_FOUND, "No available visit of the id " + visitID + " found");
+        }
+
+        AvailableVisit availableVisit = optionalAvailableVisit.get();
+        ScheduledVisit scheduledVisit = new ScheduledVisit();
+        Patient patient = patientService.getByUsername(username).get();
+
+        scheduledVisit.setPatient(patient);
+        scheduledVisit.setDate(availableVisit.getDate());
+        scheduledVisit.setDoctor(availableVisit.getDoctor());
+        scheduledVisit.setDuration(availableVisit.getDuration());
+        scheduledVisit.setSpecialization(availableVisit.getDoctor().getSpecialization());
+
+        scheduledVisitRepository.save(scheduledVisit);
+        availableVisitRepository.delete(availableVisit);
+
+        sendScheduledVisitNotification(patient, availableVisit);
+
+        return new Response(true, HttpStatus.OK, "Successfully scheduled visit");
+    }
+
+    /**
+     * Returns available visits for the illnesses added to the account
+     * @param username username of the user for whom available visits are checked
+     * @return response with status of the performed action
+     */
+    public Map<String, List<AvailableVisit>> checkVisitRecommendations(String username) {
+        Optional<Patient> patient = patientService.getByUsername(username);
+
+        if (patient.isEmpty())
+            return Collections.emptyMap();
+
+        Set<Illness> illnesses = patient.get().getIllnesses();
+
+        return illnesses.stream()
+                .collect(Collectors.toMap(Illness::getName, illness -> getAvailableVisits(illness.getName()).get()));
+
+    }
+
+    /**
+     * Returns a list containing scheduled visits for the account of the provided username
+     * @param username username of the account to have scheduled visits checked
+     * @return a list containing scheduled visits
+     */
+    public List<ScheduledVisit> checkScheduledVisits(String username) {
+        Optional<Doctor> doctor = doctorService.getByUsername(username);
+
+        if (doctor.isEmpty())
+            return Collections.emptyList();
+
+        return doctor.get().getScheduledVisits();
+    }
+
+    /**
+     * Retrieves all the scheduled visits for a doctor of the specified username
+     * @param username username of the Doctor to have visits retrieved
+     * @return list of ScheduledVisit instances
+     */
+    public List<ScheduledVisit> getScheduledVisitsForToday(String username) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return checkScheduledVisits(username).stream().filter(visit -> visit.getDate().format(format).equals(LocalDateTime.now().format(format))).toList();
+    }
+
+    /**
+     * Adds available visits for a doctor account
+     * @param username username of the doctor account who has visits added
+     * @param visits a list of visits to be added
+     */
+    @Transactional
+    public void addVisits(String username, List<VisitRequest> visits) {
+        Doctor doctor = doctorService.getByUsername(username).get();
+
+        List<AvailableVisit> availableVisits = new ArrayList<>(doctor.getAvailableVisits());
+
+        for (VisitRequest visit : visits) {
+            AvailableVisit availableVisit = new AvailableVisit();
+            availableVisit.setDoctor(doctor);
+            availableVisit.setDate(visit.getDate());
+            availableVisit.setDuration(visit.getDuration());
+            availableVisits.add(availableVisit);
+        }
+
+        doctor.setAvailableVisits(availableVisits);
+    }
+
+    /**
+     * Auxiliary method for notifying via email when a new visit is scheduled
+     * @param patient patient who scheduled the visit
+     * @param visit available visit that was scheduled
+     */
+    private void sendScheduledVisitNotification(Patient patient, AvailableVisit visit) {
+        String message = "Successfully scheduled visit to " + visit.getDoctor().getUser().fullName() +
+                " for " + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(visit.getDate()) +
+                "\nThe visit will take approximately " + visit.getDuration() + " minutes";
+
+        mailSenderService.sendNewMail(patient.getUser().getEmail(), "New visit scheduled", message);
+    }
 }
