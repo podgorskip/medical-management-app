5a6673501f3c5fb61f28dc8176ea01d4a1480ba6
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-17 23:32:14 +0100

    Adds new entity representing question

--- src/main/java/app/registrationSystem/jpa/entities/Question.java
+++ src/main/java/app/registrationSystem/jpa/entities/Question.java
@@ -1,2 +1,38 @@
-package app.registrationSystem.jpa.entities;public class Question {
+package app.registrationSystem.jpa.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "question", schema = "registration_system")
+@Data
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "specialization_id")
+    private Specialization specialization;
+
+    private String question;
+
+    @OneToOne(mappedBy = "question")
+    private Answer answer;
+
+    @Column(name = "question_date")
+    private LocalDateTime questionDate;
 }
