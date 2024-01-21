c526b6643e638011d53b145659af172e2a1d557c
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-21 12:17:49 +0100

    Implements repository to access verification code entities

--- src/main/java/app/registrationSystem/jpa/repositories/VerificationCodeRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/VerificationCodeRepository.java
@@ -1,2 +1,12 @@
-package app.registrationSystem.jpa.repositories;public class VerificationCodeRepository {
+package app.registrationSystem.jpa.repositories;
+
+import app.registrationSystem.jpa.entities.User;
+import app.registrationSystem.jpa.entities.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByUser(User user);
 }
