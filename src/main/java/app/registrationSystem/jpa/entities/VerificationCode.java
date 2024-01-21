da9682371adb24bd87225cc03fdeb1b17bc15f0a
Patryk Podgórski <podgorski.p@interia.eu>
2024-01-21 12:17:21 +0100

    Implements entity that represents verification code

--- src/main/java/app/registrationSystem/jpa/entities/VerificationCode.java
+++ src/main/java/app/registrationSystem/jpa/entities/VerificationCode.java
@@ -1,2 +1,22 @@
-package app.registrationSystem.jpa.entities;public class VerificationCode {
+package app.registrationSystem.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "verification_code", schema = "registration_system")
+@Data
+public class VerificationCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String code;
+
+    private LocalDateTime issueDate;
 }
