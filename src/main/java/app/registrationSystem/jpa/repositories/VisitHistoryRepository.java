2925b1000ca5f009f1053e80b51c1c7874f2bbff
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:23:50 +0100

    Adds method returning visit histories by doctor

--- src/main/java/app/registrationSystem/jpa/repositories/VisitHistoryRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/VisitHistoryRepository.java
@@ -1,5 +1,6 @@
 package app.registrationSystem.jpa.repositories;
 
+import app.registrationSystem.jpa.entities.Doctor;
 import app.registrationSystem.jpa.entities.Patient;
 import app.registrationSystem.jpa.entities.VisitHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,8 @@ public interface VisitHistoryRepository extends JpaRepository<VisitHistory, Long
 
     @Query("SELECT vh FROM VisitHistory vh " +
             "LEFT JOIN vh.review r " +
-            "WHERE r IS NULL")
+            "WHERE r IS NULL AND vh.patient = :patient")
     Optional<List<VisitHistory>> findUnreviewedByPatient(Patient patient);
+
+    Optional<List<VisitHistory>> findByDoctor(Doctor doctor);
 }
