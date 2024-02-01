0537067609abece2118c43a555b322e4d1f06467
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:36:30 +0100

    Adds annotation to connect with front end application

--- src/main/java/app/registrationSystem/RegistrationSystemApplication.java
+++ src/main/java/app/registrationSystem/RegistrationSystemApplication.java
@@ -2,8 +2,10 @@ package app.registrationSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class RegistrationSystemApplication {
 
 	public static void main(String[] args) {
