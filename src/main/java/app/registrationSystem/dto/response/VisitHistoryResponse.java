368e0041052441b834174057e7281acb0e0b88a8
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:48:33 +0100

    Adds visit history response

--- src/main/java/app/registrationSystem/dto/response/VisitHistoryResponse.java
+++ src/main/java/app/registrationSystem/dto/response/VisitHistoryResponse.java
@@ -1,2 +1,13 @@
-package app.registrationSystem.dto.response;public class VisitHistoryResponse {
+package app.registrationSystem.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class VisitHistoryResponse {
+    private Long id;
+    private String patient;
+    private String doctor;
+    private LocalDateTime date;
+    private String specialization;
 }
