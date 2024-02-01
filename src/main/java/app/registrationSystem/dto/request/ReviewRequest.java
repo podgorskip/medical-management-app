5cc07f9631751a57313f9e261b833b6548ae5c82
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:47:12 +0100

    Implements review request

--- src/main/java/app/registrationSystem/dto/request/ReviewRequest.java
+++ src/main/java/app/registrationSystem/dto/request/ReviewRequest.java
@@ -1,2 +1,14 @@
-package app.registrationSystem.dto.request;public class ReviewRequest {
+package app.registrationSystem.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+@Data
+public class ReviewRequest {
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 10, message = "Rating cannot be greater than 10")
+    private int rating;
+
+    private String comment;
 }
