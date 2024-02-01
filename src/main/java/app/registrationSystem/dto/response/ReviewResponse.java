a19355014d5ecf2afbcd4b2f35d4419443658646
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:47:59 +0100

    Adds review response

--- src/main/java/app/registrationSystem/dto/response/ReviewResponse.java
+++ src/main/java/app/registrationSystem/dto/response/ReviewResponse.java
@@ -1,2 +1,14 @@
-package app.registrationSystem.dto.response;public class ReviewResponse {
+package app.registrationSystem.dto.response;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ReviewResponse {
+    private String patient;
+    private String doctor;
+    private int rating;
+    private String comment;
+    private LocalDateTime visitDate;
+    private LocalDateTime reviewDate;
 }
