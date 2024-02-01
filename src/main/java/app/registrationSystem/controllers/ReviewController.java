42e2c285d981b2c0a893ccab379a1ad4e09ee84d
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:19:43 +0100

    Adds endpoint returning reviews by doctor

--- src/main/java/app/registrationSystem/controllers/ReviewController.java
+++ src/main/java/app/registrationSystem/controllers/ReviewController.java
@@ -1,17 +1,23 @@
 package app.registrationSystem.controllers;
 
+import app.registrationSystem.dto.mappers.ReviewMapper;
+import app.registrationSystem.dto.mappers.VisitHistoryMapper;
+import app.registrationSystem.dto.request.ReviewRequest;
+import app.registrationSystem.dto.response.Response;
+import app.registrationSystem.dto.response.ReviewResponse;
+import app.registrationSystem.dto.response.VisitHistoryResponse;
+import app.registrationSystem.jpa.entities.Review;
 import app.registrationSystem.jpa.entities.VisitHistory;
 import app.registrationSystem.security.CustomUserDetails;
 import app.registrationSystem.security.Privilege;
 import app.registrationSystem.security.RequiredPrivilege;
+import app.registrationSystem.services.ReviewService;
 import app.registrationSystem.services.VisitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,15 +26,32 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class ReviewController {
     private final VisitService visitService;
+    private final ReviewService reviewService;
 
     @RequiredPrivilege(value = Privilege.REVIEW_VISIT)
-    @GetMapping("/to-review")
-    public ResponseEntity<List<VisitHistory>> checkVisitsToReview(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    @GetMapping("/patient/to-review")
+    public ResponseEntity<List<VisitHistoryResponse>> checkVisitsToReview(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         Optional<List<VisitHistory>> visitHistories = visitService.getVisitsWaitingForReview(customUserDetails.getUsername());
 
-        if (visitHistories.isEmpty())
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return visitHistories
+                .map(histories -> ResponseEntity.status(HttpStatus.OK).body(histories.stream().map(VisitHistoryMapper.INSTANCE::convert).toList()))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 
-        return ResponseEntity.status(HttpStatus.OK).body(visitHistories.get());
+    }
+
+    @RequiredPrivilege(value = Privilege.REVIEW_VISIT)
+    @PostMapping("/patient/review")
+    public ResponseEntity<Response> reviewVisit(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam("id") Long id, @RequestBody ReviewRequest reviewRequest) {
+        Response response = reviewService.reviewVisit(id, customUserDetails.getUsername(), reviewRequest);
+        return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+
+    @GetMapping("/auth/reviews")
+    public ResponseEntity<List<ReviewResponse>> checkReviews(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam("id") Long id) {
+        Optional<List<Review>> reviews = reviewService.getByDoctor(id);
+
+        return reviews
+                .map(reviewList -> ResponseEntity.status(HttpStatus.OK).body(reviewList.stream().map(ReviewMapper.INSTANCE::convert).toList()))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
