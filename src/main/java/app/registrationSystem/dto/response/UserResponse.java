851d8e8f748162de92b26c878ca8f0f278a2dae2
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:48:15 +0100

    Updates user response

--- src/main/java/app/registrationSystem/dto/response/UserResponse.java
+++ src/main/java/app/registrationSystem/dto/response/UserResponse.java
@@ -1,2 +1,12 @@
-package app.registrationSystem.dto.response;public class UserResponse {
+package app.registrationSystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserResponse {
+    private String fullName;
+    private String email;
+    private String phoneNumber;
 }
