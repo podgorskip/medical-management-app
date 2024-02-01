64d43a3bd6edadc70a0f6ded555bc45aa0b7e1d8
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:22:43 +0100

    Adds method returning review by visit history

--- src/main/java/app/registrationSystem/jpa/repositories/ReviewRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/ReviewRepository.java
@@ -1,2 +1,12 @@
-package app.registrationSystem.jpa.repositories;public class ReviewRepository {
+package app.registrationSystem.jpa.repositories;
+
+import app.registrationSystem.jpa.entities.Review;
+import app.registrationSystem.jpa.entities.VisitHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Optional<Review> findByVisitHistory(VisitHistory visitHistory);
 }
