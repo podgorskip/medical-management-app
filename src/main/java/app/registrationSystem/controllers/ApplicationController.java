6695d4c56fae6804935ee81bad415cb177eaba98
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:16:57 +0100

    Adds enpoint returning user details

--- src/main/java/app/registrationSystem/controllers/ApplicationController.java
+++ src/main/java/app/registrationSystem/controllers/ApplicationController.java
@@ -7,6 +7,8 @@ import app.registrationSystem.dto.response.IllnessResponse;
 import app.registrationSystem.dto.response.Response;
 import app.registrationSystem.dto.request.UserRegistrationRequest;
 import app.registrationSystem.dto.response.SpecializationResponse;
+import app.registrationSystem.dto.response.UserResponse;
+import app.registrationSystem.jpa.entities.User;
 import app.registrationSystem.security.CustomUserDetails;
 import app.registrationSystem.security.Privilege;
 import app.registrationSystem.security.RequiredPrivilege;
@@ -20,6 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -34,15 +37,14 @@ public class ApplicationController {
         return ResponseEntity.status(HttpStatus.OK).body(illnessService.getAll().stream().map(IllnessMapper.INSTANCE::convert).toList());
     }
 
-    @RequiredPrivilege(value = Privilege.CHECK_SPECIALIZATIONS)
-    @GetMapping("/specializations")
+    @GetMapping("/auth/specializations")
     public ResponseEntity<List<SpecializationResponse>> getAllSpecializations() {
         return ResponseEntity.status(HttpStatus.OK).body(specializationService.getAll().stream().map(SpecializationMapper.INSTANCE::convert).toList());
     }
 
     @RequiredPrivilege(value = Privilege.CHANGE_CREDENTIALS)
     @PatchMapping("/change-credentials")
-    public ResponseEntity<Response> changeCredentials(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody UserRegistrationRequest user) {
+    public ResponseEntity<Response> changeCredentials(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody @Valid UserRegistrationRequest user) {
         Response response = userService.changeCredentials(customUserDetails.getUsername(), user);
         return ResponseEntity.status(response.httpStatus()).body(response);
     }
@@ -52,6 +54,19 @@ public class ApplicationController {
     public ResponseEntity<Response> changePassword(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody @Valid PasswordChangeRequest passwordChangeRequest) {
         Response response = userService.changePassword(customUserDetails.getUsername(), passwordChangeRequest);
         return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+
+    @RequiredPrivilege(value = Privilege.CHECK_CREDENTIALS)
+    @GetMapping("/details")
+    public ResponseEntity<UserResponse> checkCredentials(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        Optional<User> optionalUser = userService.findByUsername(customUserDetails.getUsername());
+
+        if (optionalUser.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        User user = optionalUser.get();
+
+        return ResponseEntity.status(HttpStatus.OK).body(new UserResponse(user.fullName(), user.getEmail(), user.getPhoneNumber()));
     }
 
 }
