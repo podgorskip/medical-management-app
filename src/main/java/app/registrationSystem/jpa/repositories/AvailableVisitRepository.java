5b003590929510d7bc8cf3c711d63c14b69fe76b
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:22:08 +0100

    Adds method returning avaialable visits by doctor

--- src/main/java/app/registrationSystem/jpa/repositories/AvailableVisitRepository.java
+++ src/main/java/app/registrationSystem/jpa/repositories/AvailableVisitRepository.java
@@ -1,7 +1,11 @@
 package app.registrationSystem.jpa.repositories;
 
 import app.registrationSystem.jpa.entities.AvailableVisit;
+import app.registrationSystem.jpa.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface AvailableVisitRepository extends JpaRepository<AvailableVisit, Long> {
+    Optional<List<AvailableVisit>> findByDoctor(Doctor doctor);
 }
