8fc9233923b53733b9247704b2570852ff39f23d
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:45:33 +0100

    Updates reviews mapper

--- src/main/java/app/registrationSystem/dto/mappers/ReviewMapper.java
+++ src/main/java/app/registrationSystem/dto/mappers/ReviewMapper.java
@@ -1,2 +1,24 @@
-package app.registrationSystem.dto.mappers;public class ReviewMapper {
+package app.registrationSystem.dto.mappers;
+
+import app.registrationSystem.dto.response.ReviewResponse;
+import app.registrationSystem.jpa.entities.Review;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ReviewMapper {
+    ReviewMapper INSTANCE = Mappers.getMapper(ReviewMapper.class);
+
+    default ReviewResponse convert(Review review) {
+        ReviewResponse reviewResponse = new ReviewResponse();
+
+        reviewResponse.setPatient(review.getVisitHistory().getPatient().getUser().fullName());
+        reviewResponse.setDoctor(review.getVisitHistory().getDoctor().getUser().fullName());
+        reviewResponse.setComment(review.getComment());
+        reviewResponse.setVisitDate(review.getVisitHistory().getDate());
+        reviewResponse.setReviewDate(review.getDate());
+        reviewResponse.setRating(review.getRating());
+
+        return reviewResponse;
+    }
 }
