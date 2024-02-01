a8766a1e460d0a34bb200d53045731529cfde38e
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-01 18:45:50 +0100

    Updates visit history mapper

--- src/main/java/app/registrationSystem/dto/mappers/VisitHistoryMapper.java
+++ src/main/java/app/registrationSystem/dto/mappers/VisitHistoryMapper.java
@@ -1,2 +1,36 @@
-package app.registrationSystem.dto.mappers;public class VisitHistoryMapper {
+package app.registrationSystem.dto.mappers;
+
+import app.registrationSystem.dto.response.VisitHistoryResponse;
+import app.registrationSystem.jpa.entities.ScheduledVisit;
+import app.registrationSystem.jpa.entities.VisitHistory;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface VisitHistoryMapper {
+    VisitHistoryMapper INSTANCE = Mappers.getMapper(VisitHistoryMapper.class);
+
+    default VisitHistoryResponse convert(VisitHistory visitHistory) {
+        VisitHistoryResponse visitHistoryResponse = new VisitHistoryResponse();
+
+        visitHistoryResponse.setId(visitHistory.getId());
+        visitHistoryResponse.setPatient(visitHistory.getPatient().getUser().fullName());
+        visitHistoryResponse.setDoctor(visitHistory.getDoctor().getUser().fullName());
+        visitHistoryResponse.setSpecialization(visitHistory.getSpecialization().getName());
+        visitHistoryResponse.setDate(visitHistory.getDate());
+
+        return visitHistoryResponse;
+    }
+
+    default VisitHistoryResponse convert(ScheduledVisit scheduledVisit) {
+        VisitHistoryResponse visitHistoryResponse = new VisitHistoryResponse();
+
+        visitHistoryResponse.setId(scheduledVisit.getId());
+        visitHistoryResponse.setPatient(scheduledVisit.getPatient().getUser().fullName());
+        visitHistoryResponse.setDoctor(scheduledVisit.getDoctor().getUser().fullName());
+        visitHistoryResponse.setSpecialization(scheduledVisit.getSpecialization().getName());
+        visitHistoryResponse.setDate(scheduledVisit.getDate());
+
+        return visitHistoryResponse;
+    }
 }
