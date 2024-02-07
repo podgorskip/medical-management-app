07a3ad5491dc1197b42cdbe04a5567e22409b6c7
Patryk Podgórski <podgorski.p@interia.eu>
2024-02-07 13:02:06 +0100

    Adds annotation to enable schedulling

--- src/main/java/app/registrationSystem/RegistrationSystemApplication.java
+++ src/main/java/app/registrationSystem/RegistrationSystemApplication.java
@@ -2,10 +2,12 @@ package app.registrationSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@EnableScheduling
 public class RegistrationSystemApplication {
 
 	public static void main(String[] args) {
